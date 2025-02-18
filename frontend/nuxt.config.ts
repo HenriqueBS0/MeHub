34faeba02db17df948761258984997698834e0d7
HenriqueBS0 <henrique.10agr@gmail.com>
2025-02-18 20:44:26 -0300

    conf: Adicionar o tailwind e prettierrc no frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -7,4 +9,10 @@ export default defineNuxtConfig({
     // Replace with your Laravel API URL
     apiUrl: "http://localhost:8000"
   },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 });
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-const { data, refresh }  = await useAsyncData('test', () => useSanctumFetch("/api/test"));
+const { data, refresh } = await useAsyncData("test", () =>
+  useSanctumFetch("/api/test"),
+);
 </script>
 
 <template>
   {{ data }}
-  <button v-on:click="() => refresh()">Regarregar</button>
+  <button
+    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+    v-on:click="() => refresh()"
+  >
+    Regarregar
+  </button>
 </template>
