240152f49e3fe05555a2f7590e8061a7415be600
HenriqueBS0 <henrique.10agr@gmail.com>
2025-02-18 21:32:58 -0300

    conf: Configurar o prettier

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-const { data, refresh } = await useAsyncData("test", () =>
-  useSanctumFetch("/api/test"),
+const { data, refresh } = await useAsyncData('test', () =>
+	useSanctumFetch('/api/test')
 );
 </script>
 
 <template>
-  {{ data }}
-  <button
-    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
-    v-on:click="() => refresh()"
-  >
-    Regarregar
-  </button>
+	{{ data }}
+	<button
+		class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+		v-on:click="() => refresh()"
+	>
+		Regarregar
+	</button>
 </template>
